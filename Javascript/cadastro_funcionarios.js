df04af53771ebc92f78ddc87deda4bfc36be1a65
Guilherme <guilhermehsilva8@gmail.com>
2025-03-03 13:11:44 -0300

    Feito visual da tela de cadastro de funcionário

--- Javascript/cadastro_funcionarios.js
+++ Javascript/cadastro_funcionarios.js
@@ -5,7 +5,7 @@ const nome = document.getElementById('nome')
 const cpf = document.getElementById('cpf')
 const registro = document.getElementById('registro')
 const sexo = document.getElementById('sexo')
-const datanasciemnto = document.getElementById('data-nascimento')
+const datanascimento = document.getElementById('data-nascimento')
 const cidade = document.getElementById('cidade')
 const email = document.getElementById('email')
 
@@ -15,15 +15,24 @@ form.addEventListener("submit", function(event){
 
     const linha = tabelaFuncionariosBody.insertRow()
     linha.innerHTML = `
-        <td>${nome.value}</td>
-        <td>${cpf.value}</td>
-        <td>${registro.value}</td>
-        <td>${sexo.value}</td>
-        <td>${datanasciemnto.value}</td>
-        <td>${cidade.value}</td>
-        <td>${email.value}</td>
+        <td class="text-center">${nome.value}</td>
+        <td class="text-center">${cpf.value}</td>
+        <td class="text-center">${registro.value}</td>
+        <td class="text-center">${sexo.value}</td>
+        <td class="text-center">${datanascimento.value}</td>
+        <td class="text-center">${cidade.value}</td>
+        <td class="text-center">${email.value}</td>
     `
     tabelaFuncionariosBody.appendChild(linha)
 
     alert("Funcionário cadastrado");
+
+    // Limpa os inputs após inserção
+    nome.value = ''
+    cpf.value = ''
+    registro.value = ''
+    sexo.value = ''
+    datanascimento.value = ''
+    cidade.value = ''
+    email.value = ''
 })
