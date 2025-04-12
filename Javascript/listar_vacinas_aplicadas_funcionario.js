be10de96532280d9ad991e41673fbf06752e7fca
Guilherme <guilhermehsilva8@gmail.com>
2025-04-12 14:39:30 -0300

    Finalizada implementação da carteirinha de vacinação com a API

--- Javascript/listar_vacinas_aplicadas_funcionario.js
+++ Javascript/listar_vacinas_aplicadas_funcionario.js
@@ -1,69 +1,69 @@
-var funcionarios = ["Pedro", "Maria", "Ana"]
+function getVacinacoesByPaciente(id){
+    return fetch(`http://localhost:3000/vacinacoes?pacienteId=${id}`)
+}
 
-var vacinas = [
-    {
-        "FuncionarioVacinado": "Pedro",
-        "FuncionarioResponsavel": "Ana",
-        "DataAplicacao": "2025-03-20 03:00:00",
-        "TipoVacina": "Vacina A"
-    },
-    {
-        "FuncionarioVacinado": "Maria",
-        "FuncionarioResponsavel": "Pedro",
-        "DataAplicacao": "2025-03-20 03:00:00",
-        "TipoVacina": "Vacina B"
-    },
-    {
-        "FuncionarioVacinado": "Ana",
-        "FuncionarioResponsavel": "Maria",
-        "DataAplicacao": "2025-03-21 03:00:00",
-        "TipoVacina": "BCG"
-    },
-    {
-        "FuncionarioVacinado": "Ana",
-        "FuncionarioResponsavel": "Maria",
-        "DataAplicacao": "2025-03-19 03:00:00",
-        "TipoVacina": "Covid-19"
-    }
-]
+function getFuncionarios(){
+    return fetch(`http://localhost:3000/funcionarios`)
+}
+
+function getFuncionarioById(id){
+    return fetch(`http://localhost:3000/funcionarios/${id}`)
+}
+
+function getVacinaById(id){
+    return fetch(`http://localhost:3000/vacinas/${id}`)
+}
 
 // Variáveis globais
 const tbodyvacinas = document.querySelector('#tabela-vacinas > tbody')
-const hoje = new Date().toLocaleDateString()
 const selectFuncionario = document.querySelector("#funcionario")
 
 // Insere todos as vacinas chumbadas de hoje na tabela
-function criaTabelaVacinas(funcionario, vacinas) {
-    let vacinasFuncionario = vacinas.filter(function(vacina){
-        if(vacina.FuncionarioVacinado == funcionario){
-            return vacina
-        }
-    })
+function criaTabelaVacinas(idFuncionario) {
+    getVacinacoesByPaciente(idFuncionario).then(response => response.json()).then(vacinacoes => {
+        vacinacoes.forEach(function(vacinacao){
+            getFuncionarioById(vacinacao.aplicadorId).then(response => response.json()).then(aplicador => {
+                getVacinaById(vacinacao.vacinaId).then(response => response.json()).then(vacina => {
+                    const linha = tbodyvacinas.insertRow();
 
-    vacinasFuncionario.forEach(function(vacina){
-        const linha = tbodyvacinas.insertRow();
-        linha.innerHTML = `
-        <td tipo="funcionario-responsavel" class="text-center">${vacina.FuncionarioResponsavel}</td>
-        <td tipo="data-aplicacao" class="text-center">${new Date(vacina.DataAplicacao).toLocaleDateString()}</td>
-        <td tipo="tipo-vacina" class="text-center">${vacina.TipoVacina}</td>
-        `
-        tbodyvacinas.appendChild(linha)
+                    const tdAplicador = document.createElement('td')
+                    tdAplicador.classList.add('text-center')
+                    tdAplicador.innerText = `${aplicador.Nome} - ${aplicador.Cpf}`
+                    linha.appendChild(tdAplicador)
+
+                    const tdData = document.createElement('td')
+                    tdData.classList.add('text-center')
+                    tdData.innerText = `${vacinacao.data}`
+                    linha.appendChild(tdData)
+
+                    const tdVacina = document.createElement('td')
+                    tdVacina.classList.add('text-center')
+                    tdVacina.innerText = `${vacina.Nome}`
+                    linha.appendChild(tdVacina)
+
+                    tbodyvacinas.appendChild(linha)
+                })
+            })
+        })
     })
 }
 
 // Cria as options do select de acordo com o array de funcionários
-funcionarios.forEach(function(funcionario){
-    let option = document.createElement('option')
-    option.innerText = funcionario
 
-    selectFuncionario.appendChild(option)
+getFuncionarios().then(result => result.json()).then(funcionarios => {
+    funcionarios.forEach(function(funcionario){
+        const option = document.createElement('option')
+        option.value = funcionario.id
+        option.innerText = `${funcionario.Nome} - ${funcionario.Cpf}`
+        selectFuncionario.appendChild(option)
+    })
 })
 
 // Toda vez que um valor do select for selecionado, será trago as vacinas daquele funcionário
 selectFuncionario.addEventListener('change', function(event){
     tbodyvacinas.innerHTML = ''
 
-    let nomeFuncionario = selectFuncionario.value
+    let idFuncionario = selectFuncionario.selectedOptions[0].value
     
-    criaTabelaVacinas(nomeFuncionario, vacinas)
+    criaTabelaVacinas(idFuncionario)
 })
